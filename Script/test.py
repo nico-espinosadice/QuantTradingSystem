7e53b5606f681f049a9ea96cd681ade8d841c395
Nico Espinosa Dice <nespinosadice@hmc.edu>
2019-04-30 15:28:30 -0700

    added script

---
+++ Script/test.py
@@ -0,0 +1,11 @@
+import alpaca_trade_api as tradeapi
+
+api = tradeapi.REST(key_id='PKXVRTVRHQWTL50AYFKA',
+                                 secret_key='SUXQAbLyKCZdJMQnvCcwnpi7aEKXq0ILydB6iYpB',
+                                 base_url='https://paper-api.alpaca.markets')
+api.submit_order(
+    symbol = "AAPL",
+    qty=1,
+    side='buy',
+    type='market',
+    time_in_force='gtc')
